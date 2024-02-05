fbe2ed370b402fed089d425851690bdb7e272266
Grazieli Francisco <129116654+GraziFrancisco@users.noreply.github.com>
2024-02-05 16:21:10 -0300

    up ex 03

--- ex-03/src/app/app.component.ts
+++ ex-03/src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  detalhe = false;
+  log = [];
+
+  addDetalhes() {
+    this.detalhe = !this.detalhe;
+    //this.log.push(this.log.length + 1)
+    this.log.push(new Date())
+  }
+
+
 }
