0c60b713f7c835ccd93adf0d4e850ee7496b99ee
Grazieli Francisco <129116654+GraziFrancisco@users.noreply.github.com>
2024-02-06 14:22:12 -0300

    up

--- ex-04/src/app/app.component.ts
+++ ex-04/src/app/app.component.ts
@@ -6,23 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{type: 'server', name: 'TesteName', content: 'Somente teste!'}];
+  
+  onServerAdded(serverData: {serverName: string, serverContent: string}) { 
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
+  
+}
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) { 
+  this.serverElements.push({
+    type: 'blueprint',
+    name: blueprintData.serverName,
+    content: blueprintData.serverContent
+  });
+  
+}
 
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  
 }
