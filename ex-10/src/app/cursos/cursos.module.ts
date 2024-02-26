257f80c67042668088a6818552c74854155eb0eb
Grazieli Francisco <129116654+GraziFrancisco@users.noreply.github.com>
2024-02-26 17:09:53 -0300

    up 26-02-criacao-de-modulo

---
+++ ex-10/src/app/cursos/cursos.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosComponent } from './cursos.component';
+
+
+
+@NgModule({
+  declarations: [
+    CursoDetalheComponent,
+    CursosComponent
+    
+  ],
+  imports: [
+    CommonModule
+  ],
+
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
